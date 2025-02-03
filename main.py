1df83d14d47d20efb119d56e922a290b8cdd0861
IanPasq <iangrofoski@gmail.com>
2025-02-02 22:35:42 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import click
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings')
+@click.option('--name', prompt='Your name', help='the person to greet')
+
+def hello(count, name):
+    for x in range(count):
+        click.echo(f"hello {name}!")
+
+if __name__ == '__main__':
+    hello()
