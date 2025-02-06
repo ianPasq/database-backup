2d6a00c8df9079b38c2356c10c6d452d1ecbae31
IanPasq <iangrofoski@gmail.com>
2025-02-06 02:32:40 -0300

    execute function

--- interface.py
+++ interface.py
@@ -2,6 +2,13 @@ import click
 import os
 from db_manager import DBManager
 
+
+@click.group()
+def cli():
+    pass
+
+
+
 @click.command()
 @click.option("--db-type", type=click.Choice(["mysql", "postgresql", "mongodb"]), require=True, help="Database type")
 @click.option("--host", default="localhost", help="Database host")
@@ -11,7 +18,7 @@ from db_manager import DBManager
 @click.option("--port", type=int, default=None, help="Database port")
 
 
-def connect(db_type, host, user, password, database. port):
+def connect(db_type, host, user, password, database, port):
     if not port:
         port = {"mysql": 3306, "postgresql": 5432, "mongodb": 27017}[db_type]
 
@@ -25,8 +32,6 @@ def connect(db_type, host, user, password, database. port):
         click.echo(f"Failed to connect to {db_type}")
     
 
-if __name__ == "__main__":
-    connect()
 
 
     # connect to mysql: python cli.py --db-type mysql --user root --password secret --database testdb --host 127.0.0.1
@@ -34,4 +39,55 @@ if __name__ == "__main__":
     # connect to postgre: python cli.py --db-type postgresql --user postgres --password secret --database mydb --host 127.0.0.1 
 
     # connect to mogodb: python cli.py --db-type mongodb --user mongo --password secret --database testdb --host 127.0.0.1
+
+
+
+@click.command()
+@click.option("--db-type", type=click.Choice(["mysql", "postgresql", "mongodb"]), required=True, help="Database type")
+@click.option("--host", default="localhost", help="Database host")
+@click.option("--user", default=lambda: os.getenv("DB_USER", "root"), help="Database username")
+@click.option("--password", default=lambda: os.getenv("DB_PASSWORD", ""), help="Database password")
+@click.option("--database", default=lambda: os.getenv("DB_NAME", ""), help="Database name")
+@click.option("--port", type=int, default=None, help="Database port")
+@click.argument("query")
+
+
+def execute(db_type, host, user, password, database, port, query):
+    if not port:
+        port = {"mysql": 3306, "postgresql": 5432, "mongodb": 27017}[db_type]
+
+    db = DBManager(db_type, host, user, password, database, port)
+    conn = db.connect()
+
+    if conn:
+        click.echo(f"Executing query on {db_type}...")
+        db.execute_query(query)
+        db.close()
+    else:
+        click.echo(f"Failed to connect to {db_type}")
+
+cli.add_command(connect)
+cli.add_command(execute)
+
+
+
+if __name__ == "__main__":
+    cli()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
